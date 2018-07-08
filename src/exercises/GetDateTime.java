0420798f3f25ec3ae1190a038cc83c76aa27a297
Mehavarnan <meha10aug@gmail.com>
2018-07-08 23:24:11 +0530

    updated code in GetDateTime class

--- src/exercises/GetDateTime.java
+++ src/exercises/GetDateTime.java
@@ -13,13 +13,15 @@ public class GetDateTime {
 		
 		System.out.println("Date ==> " + date.toString());	
 		
-		Timestamp ts = new Timestamp(time);
+		//Timestamp ts = new Timestamp(time);
+		Timestamp ts = new Timestamp(System.currentTimeMillis());
 		System.out.println("Timestamp ==> " + ts.toString());
 		
-		Date date1 = new Date();
-		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss a");
-		String formattedDate = sdf.format(date1);
-		System.out.println(formattedDate);
+		
+//		Date date1 = new Date();
+//		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss a");
+//		String formattedDate = sdf.format(date1);
+//		System.out.println(formattedDate);
 
 	}
 
