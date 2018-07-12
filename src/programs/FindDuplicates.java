eeb36b05b73120ec77fa38d0afd565c4f6b115c1
Mehavarnan <meha10aug@gmail.com>
2018-07-12 12:26:30 +0530

    Adding Find Duplicates

---
+++ src/programs/FindDuplicates.java
@@ -0,0 +1,39 @@
+package programs;
+
+import java.util.ArrayList;
+
+public class FindDuplicates {
+
+	public static void main(String[] args) {
+
+		int[] a = { 1, 2, 3, 2, 4, 4, 5, 5, 6, 7 };
+
+		ArrayList<Integer> dup = new ArrayList<Integer>();
+		ArrayList<Integer> nonDup = new ArrayList<Integer>();
+
+		int flag;
+
+		for (int i = 0; i < a.length; i++) {
+			flag = 0;
+			for (int j = 0; j < a.length; j++) {
+				if (i != j) {
+					if (a[i] == a[j]) {
+						if (!dup.contains(a[j])) {
+							dup.add(a[j]);
+							flag = 1;
+						}else {
+							flag = 1;
+						}
+					}
+				}
+			}
+			if (flag == 0) {
+				nonDup.add(a[i]);
+			}
+		}
+
+		System.out.println("Duplicated Values: " + dup);
+		System.out.println("Non-Duplicated Values: " + nonDup);
+	}
+
+}
