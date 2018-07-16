4843c4d43b9148f0ac03b5a615c1edf4360aa315
Mehavarnan <meha10aug@gmail.com>
2018-07-16 17:59:40 +0530

    Updated FileClass

--- src/understandingclasses/FileClass.java
+++ src/understandingclasses/FileClass.java
@@ -2,6 +2,9 @@ package understandingclasses;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.UUID;
+
+import org.apache.poi.hwpf.model.FibRgLw;
 
 public class FileClass {
 	public static void main(String[] args) throws IOException {
@@ -10,8 +13,11 @@ public class FileClass {
 		 * accepts FileName or DirectoryName as argument 
 		 * creates just a file instance
 		 */
-		File f = new File("C:\\Users\\MEHAVARNAN\\Documents\\TestFolder\\mkdir\\");
-
+		File f = new File("C:\\Users\\MEHAVARNAN\\Documents\\TestFolder");
+		
+		File[] list = f.listFiles();
+		System.out.println(list.length);
+		
 		// creates a new file if not exists
 		// f.createNewFile();
 
