de62fd7d31775c7e152bd72472e1727dd41d4290
Mehavarnan <meha10aug@gmail.com>
2018-07-17 16:58:56 +0530

    Added casting class

---
+++ src/casting/Casting.java
@@ -0,0 +1,44 @@
+package casting;
+
+class Animal 
+{ 
+    public void callme()
+    {
+        System.out.println("In callme of Animal");
+    }
+    
+    public void func()
+    {
+        System.out.println("Animal Specific FUnction");
+    }
+}
+
+
+class Dog extends Animal 
+{ 
+    public void callme()
+    {
+        System.out.println("In callme of Dog");
+    }
+
+    public void callme2()
+    {
+        System.out.println("In callme2 of Dog");
+    }
+}
+
+public class Casting 
+{
+    public static void main (String [] args) 
+    {
+        Animal animal = new Animal();
+        //Dog dog = (Dog)animal; //ClassCastException
+        
+        if(animal instanceof Dog) {
+        	System.out.println("animal can be casted to Dog");
+        }else {
+        	System.out.println("animal cannot be casted to Dog");
+        }
+        
+    }
+}
