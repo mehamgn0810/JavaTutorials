c20e8a13032bbfc672111250eed09fdd63bb4139
Mehavarnan <meha10aug@gmail.com>
2018-07-25 22:59:56 +0530

    Added JoinAndSynchronized

---
+++ src/threads/JoinAndSynchronized.java
@@ -0,0 +1,47 @@
+package threads;
+
+public class JoinAndSynchronized {
+	private static int count = 0;
+	
+	public static synchronized void inccount() {
+		count++;
+	}
+
+	public static void main(String[] args) {
+
+		Thread t1 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				for (int i = 0; i < 10000; i++) {
+					//count++;
+					inccount();
+				}
+			}
+		});
+
+		Thread t2 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				for (int i = 0; i < 10000; i++) {
+					//count++;
+					inccount();
+				}
+			}
+		});
+
+		t1.start();
+		t2.start();
+		
+		try {
+			t1.join();
+			t2.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.err.println(count);
+	}
+
+}
