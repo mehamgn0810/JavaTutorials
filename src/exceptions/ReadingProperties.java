1ea240b5bd93dce673bf74be2fc29bb2fb4c9f0e
Mehavarnan <meha10aug@gmail.com>
2018-09-06 08:46:37 +0530

    updated readproperties

--- src/exceptions/ReadingProperties.java
+++ src/exceptions/ReadingProperties.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class ReadingProperties {
-// Please change the extension to .java before running the file
-// Udemy does not allow to upload .java file, that’s why I have to change the extension
 
 	public static void main(String[] args) throws IOException {
 		String path = "//Users//atomar//Documents//workspace_personal//JavaTutorial//src/test.properties";
